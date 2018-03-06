b5044cc5517775f43a8fe352078cfc9fa00eaa13
Jason Welsh <jasonbwelsh@gmail.com>
2018-03-06 14:39:39 -0500

    data object returned form server

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('Hi there!');
+
+const testBtn = document.getElementById('testBtn');
+testBtn.addEventListener('click', loadData);
+
+
+function loadData () {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://api.fixer.io/latest', true);
+
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      console.log(xhr.responseText);
+    }
+  }
+
+  xhr.send();
+}
